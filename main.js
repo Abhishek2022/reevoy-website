c2013eb81470036a42defa16f0226bead4969131
Abhishek2022 <avishekprasad26@gmail.com>
2020-10-27 08:13:58 +0530

    laptop-transition added

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#cf7_controls").on('mouseover', 'li', function() {
+        $("#cf7 img").removeClass("opaque");
+        
+        var newImage = $(this).index();
+        
+        $("#cf7 img").eq(newImage).addClass("opaque");
+        $("#cf7 img").eq(newImage).slideUp();
+
+        $("#cf7_controls").removeClass("selected");
+        $(this).addClass("selected");
+    });
+});
