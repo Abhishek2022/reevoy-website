0581fa00a935bf9bb03f8ebfa200e6d77d2a1d6e
Abhishek2022 <avishekprasad26@gmail.com>
2020-11-28 22:51:17 +0530

    form test php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,6 +1,17 @@
 <?php
     if(isset($_POST['btn-send']))
     {
-        echo 'Working';
+        $Iec = $_POST['iec'];
+        $Phone = $_POST['phone'];
+        $Email = $_POST['email'];
+        $Name = $_POST['name'];
+
+        $mailTo = "avishekprasad26@gmail.com"
+        $subject = $Email;
+        $txt = $Iec;
+        $headers = $Phone
+
+        mail($mailTo, $subject, $txt, $headers);
+        
     }
 ?>
